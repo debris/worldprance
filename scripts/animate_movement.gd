75354c392cecc4c5f469666717a4b08dd78ca021
Marek Kotewicz <marek.kotewicz@gmail.com>
2024-06-19 16:46:37 +0200

    jump and double jump

--- scripts/animate_movement.gd
+++ scripts/animate_movement.gd
@@ -1,11 +1,20 @@
 extends Node
 class_name AnimateMovement
 
+@export var character: CharacterBody2D
 @export var animation_tree: AnimationTree
 @onready var animation_state = animation_tree.get("parameters/playback")
 
 func _process(_delta):
 	var direction = Input.get_axis("ui_left", "ui_right")
+
+	if not character.is_on_floor():
+		if character.double_jumped:
+			animation_state.travel("DoubleJump")
+		else:
+			animation_state.travel("Jump")
+		return
+
 	if direction == 0:
 		animation_state.travel("Idle")
 	else:
--- scripts/player.gd
+++ scripts/player.gd
@@ -6,16 +6,22 @@ const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+var double_jumped = false
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+	else:
+		double_jumped = false
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("ui_accept"):
+		if is_on_floor():
+			velocity.y = JUMP_VELOCITY
+		elif !double_jumped:
+			velocity.y += JUMP_VELOCITY
+			double_jumped = true
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
