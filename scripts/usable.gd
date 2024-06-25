acf2135b61e6984a16ea30b289a437fdeffb1393
Marek Kotewicz <marek.kotewicz@gmail.com>
2024-06-25 15:45:44 +0200

    press to use label

--- scripts/usable.gd
+++ scripts/usable.gd
@@ -8,12 +8,20 @@ signal used
 var players_in_range: Dictionary
 
 func _ready():
+	var press_to_use = preload("res://scenes/ui/press_to_use.tscn").instantiate()
+	press_to_use.visible = false
+	area.add_child(press_to_use)
+	press_to_use.position = Vector2(8.0, -8.0)
+
 	area.body_entered.connect(func(player):
 		players_in_range[player] = null
+		press_to_use.visible = true
 	)
 
 	area.body_exited.connect(func(player):
 		players_in_range.erase(player)
+		if players_in_range.is_empty():
+			press_to_use.visible = false
 	)
 
 func _input(event):
