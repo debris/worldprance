846f28d5e66257458aa3e42158914fb706fb7afd
Marek Kotewicz <marek.kotewicz@gmail.com>
2024-07-03 16:31:19 +0200

    removed unused map objects

--- resources/map/game_level.gd
+++
@@ -1,9 +0,0 @@
-extends Resource
-class_name GameLevel
-
-@export var left: String
-@export var up: String
-@export var right: String
-@export var down: String
-@export var dimension: String
-@export var size: Vector2
