c29c7aafa8575d128bc79845b1d0a79389cf2f65
Marek Kotewicz <marek.kotewicz@gmail.com>
2024-07-03 11:11:15 +0200

    more level select blocks

--- scenes/ui/select_level.gd
+++ scenes/ui/select_level.gd
@@ -3,7 +3,8 @@ extends Node2D
 const LEVELS_PATH = "res://resources/levels"
 
 @onready var levels_grid = $GridContainer/GridContainer/ScrollContainer/LevelsGrid
-@onready var right_panel = $GridContainer/GridContainer/RightPanel
+#@onready var right_panel = $GridContainer/GridContainer/RightPanel
+@onready var right_panel = $RightPanel
 
 func get_levels() -> Array:
 	var result = []
@@ -19,13 +20,14 @@ func get_levels() -> Array:
 			filename = dir.get_next()
 			
 	result.sort_custom(func(a, b):
-		return a.order_id > b.order_id	
+		return a.order_id < b.order_id	
 	)
 
 	return result
 
 func _ready():
 	for level_data in get_levels():
+		print(level_data.level_name)
 		var cell = preload("res://scenes/ui/select_level_cell.tscn").instantiate()
 		cell.level_data = level_data
 		cell.cell_selected.connect(cell_selected)
